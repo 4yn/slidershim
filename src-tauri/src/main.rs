08186c0fc105eb4f8872d165fe1bc6f59ea7afad
4yn <changsiyuan1999@gmail.com>
2022-01-27 21:13:34 +0800

    system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,57 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{
+  CustomMenuItem, Event, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
+};
+
 fn main() {
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .system_tray(
+      SystemTray::new().with_menu(
+        SystemTrayMenu::new()
+          .add_item(CustomMenuItem::new("slidershim".to_string(), "slidershim").disabled())
+          .add_item(CustomMenuItem::new("show".to_string(), "Show"))
+          .add_item(CustomMenuItem::new("quit".to_string(), "Quit")),
+      ),
+    )
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::LeftClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        app
+            .get_window("main")
+            .unwrap()
+            .show().ok();
+      }
+      SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+        "show" => {
+          app
+            .get_window("main")
+            .unwrap()
+            .show().ok();
+        }
+        "quit" => {
+          std::process::exit(0);
+        }
+        _ => {
+          panic!("Unexpected menu item click {}", id.as_str());
+        }
+      },
+      _ => {}
+    })
+    .build(tauri::generate_context!())
+    .expect("error while running tauri application")
+    .run(|app_handle, event| match event {
+      Event::CloseRequested { label, api, .. } if label.as_str() == "main" => {
+        api.prevent_close();
+        app_handle
+          .get_window("main")
+          .unwrap()
+          .hide().ok();
+      }
+      _ => {}
+    });
 }
